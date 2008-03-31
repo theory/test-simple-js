07645059e77a6523c77d02c0f14c082898a38536
David E. Wheeler <david@justatheory.com>
2008-03-31 22:35:42 +0000

    * Restored missing files that Marvin forgot to commit to the old repository.  * Set `svn:keywords` on the class files.

---
+++ tests/builder_death.js
@@ -0,0 +1,24 @@
+JSAN.addRepository('../lib').use('Test.Builder');
+
+var T = new Test.Builder();
+
+T.plan({ tests: 3 });
+T.diag("Testing for IE6 try/catch/Error load confusion");
+
+try { Test.Builder.die("foo"); }
+catch (ex) {
+    T.ok((ex.message === "foo"), "expected 'foo', got '" + ex.message + "'");
+}
+
+try { Test.Builder.die("bar"); }
+catch (ex) {
+    T.ok((ex.message === "bar"), "expected 'bar', got '" + ex.message + "'");
+}
+
+try { Test.Builder.die("baz"); }
+catch (ex) {
+    T.ok((ex.message === "baz"), "expected 'baz', got '" + ex.message + "'");
+}
+
+// Force ending because IE6 needs coercion.
+T._ending(); 
